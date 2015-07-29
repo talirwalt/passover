1ae3048641d870c54151673175e72942a212e6f4
Prelang Builder <builder@prelang.com>
2015-07-29 22:05:04 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Course' to allow for create, update

--- app/admin/course.rb
+++ app/admin/course.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Course do
+  permit_params :course_name
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
